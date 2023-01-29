329319209df71b35346e295d3d8d3502f6267a33
syeddelwar <delwar.mmc@gmail.com>
2023-01-28 23:31:16 -0500

    make it better

--- config/database.js
+++ config/database.js
@@ -3,11 +3,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', 'containers-us-west-23.railway.app'),
-      port: env.int('DATABASE_PORT', 5804),
+      host: env('DATABASE_HOST', 'containers-us-west-63.railway.app'),
+      port: env.int('DATABASE_PORT', 5899),
       database: env('DATABASE_NAME', 'railway'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', 'KIvi5bSRRIHKYdJs4tBL'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'QUdVLL9JPSYrtFgjIN6b'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
